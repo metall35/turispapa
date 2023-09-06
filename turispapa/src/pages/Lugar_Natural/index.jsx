217e7af7a0a18e3050371af39521d213bcc6a375
Mateo Espinosa <mateoespinosa7777@gmail.com>
2023-09-06 00:39:06 -0500

    Implementación Grid basic para L.N

--- turispapa/src/pages/Lugar_Natural/index.jsx
+++ turispapa/src/pages/Lugar_Natural/index.jsx
@@ -1,7 +1,23 @@
 
 function Lugar_Natural() {
     return (
-        <div>Lugar_Natural</div>
+        <section className="w-[80%] flex flex-col items-center mb-4">            
+            <h1 className=" text-4xl font-bold mb-4 text-[#14A44D]">
+                Galería de Imagenes
+            </h1>
+
+            <div className="grid grid-cols-4 gap-4">
+                <div className="bg-green-400  rounded h-[200px] col-span-2">Hola</div>
+                
+                <div className="bg-green-400  rounded h-[200px] w-[150px] ">Hola</div>
+
+                <div className="bg-green-400  rounded col-span-2 row-span-6">Hola</div>
+                <div className="bg-green-400  rounded h-[200px] col-span-2">Hola</div>
+                <div className="bg-green-400  rounded h-[200px] col-span-2 row-span-5">Hola</div>
+
+                <div className="bg-green-400  rounded h-[200px] col-span-4">Hola</div>
+            </div>
+        </section>
     )
 }
 
