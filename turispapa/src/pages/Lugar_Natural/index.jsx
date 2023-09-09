15b0ac0125a2d0ac06597e6e2470c3da775ad0f5
Mateo Espinosa <mateoespinosa7777@gmail.com>
2023-09-08 22:20:33 -0500

    Implementación Galería de lugares (Faltán detalles)

--- turispapa/src/pages/Lugar_Natural/index.jsx
+++ turispapa/src/pages/Lugar_Natural/index.jsx
@@ -1,24 +1,195 @@
+import { useState } from 'react'
+import IglesiaGrid from '../../assets/img/IglesiaGrid.jpg'
+import ChapGrid from '../../assets/img/ChapGrid.jpg'
+import estatua from '../../assets/img/estatua.jpg'
+import CampGrid from '../../assets/img/CampGrid.jpg'
 
 function Lugar_Natural() {
+    const [hovered, setHovered] = useState(false);
+
+    const handleHover = () => {
+        setHovered(true);
+      };
+    
+      const handleLeave = () => {
+        setHovered(false);
+      };
+
     return (
-        <section className="w-[80%] flex flex-col items-center mb-4">            
-            <h1 className=" text-4xl font-bold mb-4 text-[#14A44D]">
-                Galería de Imagenes
-            </h1>
+      <section className="w-[50%] flex flex-col items-center mb-4">
+        <h1 className=" text-4xl font-bold mb-4 text-[#14A44D]">
+          Galería de Imagenes
+        </h1>
 
-            <div className="grid grid-cols-4 gap-4">
-                <div className="bg-green-400  rounded h-[200px] col-span-2">Hola</div>
-                
-                <div className="bg-green-400  rounded h-[200px] w-[150px] ">Hola</div>
+        <div className="grid grid-cols-4 gap-4">
+          <div
+            className="rounded h-[200px] col-span-2 overflow-hidden relative"
+            onMouseEnter={handleHover}
+            onMouseLeave={handleLeave}
+          >
+            <img className="h-[100%] w-[100%] object-cover" src={IglesiaGrid} />
 
-                <div className="bg-green-400  rounded col-span-2 row-span-6">Hola</div>
-                <div className="bg-green-400  rounded h-[200px] col-span-2">Hola</div>
-                <div className="bg-green-400  rounded h-[200px] col-span-2 row-span-5">Hola</div>
-
-                <div className="bg-green-400  rounded h-[200px] col-span-4">Hola</div>
+            <div
+              className={`w-[100%] h-[100%] top-0 text-[#fff] absolute bg-[#1f3d4738] backdrop-blur-sm p-[30px] flex flex-col   items-center transition-opacity ${
+                hovered ? "opacity-100" : "opacity-0"
+              }`}
+            >
+              <h1 className="uppercase text-xl font-semibold">
+                Iglesia Nuestra Señora de las mercedes
+              </h1>
+              <p className="capitalize text-sm font-medium ">
+                Iglesia de La Unión.
+              </p>
             </div>
-        </section>
-    )
+          </div>
+
+          <div
+            className="rounded h-[200px] col-span-1 overflow-hidden relative  "
+            onMouseEnter={handleHover}
+            onMouseLeave={handleLeave}
+          >
+            <img className="h-[100%] w-[100%] object-cover" src={ChapGrid} />
+
+            <div
+              className={`w-[100%] h-[100%] top-0 text-[#fff] absolute bg-[#1f3d4738] backdrop-blur-sm p-[30px] flex flex-col   items-center transition-opacity ${
+                hovered ? "opacity-100" : "opacity-0"
+              }`}
+            >
+              <h1 className="uppercase text-2xl font-semibold">Chapeco</h1>
+              <p className="capitalize text-sm font-medium">
+                Lugar de Homenaje.
+              </p>
+            </div>
+          </div>
+
+          <div
+            className="rounded h-[200px] col-span-1 overflow-hidden relative  "
+            onMouseEnter={handleHover}
+            onMouseLeave={handleLeave}
+          >
+            <img className="h-[100%] w-[100%] object-cover" src={ChapGrid} />
+
+            <div
+              className={`w-[100%] h-[100%] top-0 text-[#fff] absolute bg-[#1f3d4738] backdrop-blur-sm p-[30px] flex flex-col   items-center transition-opacity ${
+                hovered ? "opacity-100" : "opacity-0"
+              }`}
+            >
+              <h1 className="uppercase text-2xl font-semibold">Chapeco</h1>
+              <p className="capitalize text-sm font-medium">
+                Lugar de Homenaje.
+              </p>
+            </div>
+          </div>
+
+          <div
+            className="rounded h-[200px] col-span-2 overflow-hidden relative  "
+            onMouseEnter={handleHover}
+            onMouseLeave={handleLeave}
+          >
+            <img className="h-[100%] w-[100%] object-cover" src={estatua} />
+
+            <div
+              className={`w-[100%] h-[100%] top-0 text-[#fff] absolute bg-[#1f3d4738] backdrop-blur-sm p-[30px] flex flex-col   items-center transition-opacity ${
+                hovered ? "opacity-100" : "opacity-0"
+              }`}
+            >
+              <h1 className="uppercase text-2xl font-semibold">
+                {" "}
+                Felix María Restrepo Londoño
+              </h1>
+              <p className="capitalize text-sm font-medium">
+                Reconocido intelectual.
+              </p>
+            </div>
+          </div>
+
+          <div
+            className="rounded col-span-2 row-span-6 overflow-hidden relative "
+            onMouseEnter={handleHover}
+            onMouseLeave={handleLeave}
+          >
+            <img className="h-[100%] w-[100%] object-cover" src={ChapGrid} />
+
+            <div
+              className={`w-[100%] h-[100%] top-0 text-[#fff] absolute bg-[#1f3d4738] backdrop-blur-sm p-[30px] flex flex-col   items-center transition-opacity ${
+                hovered ? "opacity-100" : "opacity-0"
+              }`}
+            >
+              <h1 className="uppercase text-2xl font-semibold">Chapeco</h1>
+              <p className="capitalize text-sm font-medium">
+                Lugar de Homenaje.
+              </p>
+            </div>
+          </div>
+
+          <div
+            className="rounded h-[200px] col-span-2 overflow-hidden relative  "
+            onMouseEnter={handleHover}
+            onMouseLeave={handleLeave}
+          >
+            <img className="h-[100%] w-[100%] object-cover" src={estatua} />
+
+            <div
+              className={`w-[100%] h-[100%] top-0 text-[#fff] absolute bg-[#1f3d4738] backdrop-blur-sm p-[30px] flex flex-col   items-center transition-opacity ${
+                hovered ? "opacity-100" : "opacity-0"
+              }`}
+            >
+              <h1 className="uppercase text-2xl font-semibold">
+                {" "}
+                Felix María Restrepo Londoño
+              </h1>
+              <p className="capitalize text-sm font-medium">
+                Reconocido intelectual.
+              </p>
+            </div>
+          </div>
+
+          <div
+            className="rounded h-[200px] col-span-2 overflow-hidden relative  "
+            onMouseEnter={handleHover}
+            onMouseLeave={handleLeave}
+          >
+            <img className="h-[100%] w-[100%] object-cover" src={estatua} />
+
+            <div
+              className={`w-[100%] h-[100%] top-0 text-[#fff] absolute bg-[#1f3d4738] backdrop-blur-sm p-[30px] flex flex-col   items-center transition-opacity ${
+                hovered ? "opacity-100" : "opacity-0"
+              }`}
+            >
+              <h1 className="uppercase text-2xl font-semibold">
+                {" "}
+                Felix María Restrepo Londoño
+              </h1>
+              <p className="capitalize text-sm font-medium">
+                Reconocido intelectual.
+              </p>
+            </div>
+          </div>
+
+          <div
+            className="rounded h-[200px] col-span-4 overflow-hidden relative  "
+            onMouseEnter={handleHover}
+            onMouseLeave={handleLeave}
+          >
+            <img className="h-[100%] w-[100%] object-cover" src={CampGrid} />
+
+            <div
+              className={`w-[100%] h-[100%] top-0 text-[#fff] absolute bg-[#1f3d4738] backdrop-blur-sm p-[30px] flex flex-col   items-center transition-opacity ${
+                hovered ? "opacity-100" : "opacity-0"
+              }`}
+            >
+              <h1 className="uppercase text-2xl font-semibold">
+                {" "}
+                Vereda Pantalio
+              </h1>
+              <p className="capitalize text-sm font-medium">
+                Campos Unitenses.
+              </p>
+            </div>
+          </div>
+        </div>
+      </section>
+    );
 }
 
 export default Lugar_Natural
