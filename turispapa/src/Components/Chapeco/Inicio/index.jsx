8c649143cda372e22789e6cd76f4bb9fae9a424d
Mateo Pereira <pereiramateo19@gmail.com>
2023-09-08 22:11:11 -0500

    estructura base chapeco

---
+++ turispapa/src/Components/Chapeco/Inicio/index.jsx
@@ -0,0 +1,33 @@
+
+
+export default function InicioChapeco() {
+    
+    return(
+        <section>
+            <section>
+                <div>
+                    <h1>
+                        TITULO
+                    </h1>
+                    <p>
+                        CONTEXTO
+                    </p>
+                </div>
+                <div>
+                    VIDEO O IMG 360
+                </div>
+            </section>
+            <section>
+                SLIDER
+            </section>
+            <section>
+                <h2>
+                    TITULO
+                </h2>
+                <p>
+                    HISTORIA TRAGEDIA
+                </p>
+            </section>
+        </section>
+    )
+}
