d7e6221f6be85a975f662f6f875c6483e198f5cd
Mateo Espinosa <mateoespinosa7777@gmail.com>
2023-09-11 13:55:06 -0500

    Prueba escalabilidad Galeria.

---
+++ turispapa/src/pages/Lugar_Natural/MyPhotos.jsx
@@ -0,0 +1,16 @@
+import iglGrid from '../../assets/imgHeader.svg';
+import logo from '../../assets/logo.svg';
+export const photos = [
+    {
+        id: 1,
+        imagen: iglGrid,
+        titulo: "Iglesia",
+        descripcion: "Iglesia de La Unión."
+    },
+    {
+        id: 2,
+        imagen: logo,
+        titulo: "Logo",
+        descripcion: "Logo de Turispapa."
+    },
+]
