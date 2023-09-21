3abcab5f44f2e4cf2f34743f3de453c525bdc6f3
metall35 <pereiramateo19@gmail.com>
2023-09-21 01:26:56 -0500

    intento de envio de data

---
+++ turispapa/src/Components/Forms/FomAsistencias/index.jsx
@@ -0,0 +1,77 @@
+import React, { useState } from "react";
+import Forms from "../../Layout/Forms";
+import Button from "../Elements/Buttons";
+import Input from "../Elements/Inputs";
+
+export default function FormAsistencia() {
+    const [inputs, setInputs] = useState({
+        Contacto: '',
+        Direccion: '',
+        Tipo: '',
+        Imagen: ''
+    })
+    const Inputs = [
+        {
+            id: 1,
+            type: 'tel',
+            name: 'Contacto',
+            placeholder: 'Ingrese el Contacto del establecimiento',
+            required: true
+        },
+        {
+            id: 2,
+            type: 'text',
+            name: 'Direccion',
+            placeholder: 'Ingrese la dirección del establecimiento',
+            required: true
+        },
+        {
+            id: 3,
+            type: 'text',
+            name: 'Tipo',
+            placeholder: 'Ingrese el tipo de establecimiento',
+            required: true
+        },
+        {
+            id: 4,
+            type: 'file',
+            name: 'Imagen',
+            placeholder: '',
+            required: true
+        },
+    ]
+
+    const onSubmit = event => {
+        event.preventDefault()
+        console.log(inputs);
+    }
+    const onChange = (e) => {
+        const [name, value] = e.target.value
+        setInputs({ ...inputs, [name]: value })
+        // console.log(e.target.value);
+    }
+    return (
+        <Forms>
+            <h1 className="text-center my-2 mb-4">Formulario de ingreso de asistencias</h1>
+            <form className="grid grid-cols-1 md:grid-cols-2 gap-3" onSubmit={onSubmit}>
+                {Inputs.map(input => (
+                    <Input
+                        key={input.id}
+                        type={input.type}
+                        name={input.name}
+                        placeholder={input.placeholder}
+                        required={input.required}
+                        onChange={onChange}
+                        value={inputs.Contacto}
+                    />
+                ))}
+                <div className={Inputs.length % 2 === 0 ? "md:col-span-2" : "flex items-center justify-center mt-6"}>
+                    <Button
+                        type={'submit'}
+                        name={'Enviar'}
+                    />
+                </div>
+            </form>
+        </Forms>
+    )
+}
