289ff0f0efe7d90f822e1a55f6228d3aaae099c9
Mateo Pereira <pereiramateo19@gmail.com>
2024-04-02 08:34:09 -0500

    update useGetData

--- src/pages/Asistencias/index.jsx
+++ src/pages/Asistencias/index.jsx
@@ -7,14 +7,14 @@ import useGetData from "../../hooks/useGetData"
 function Asistencias() {
   const { setImageNav } = useContext(TurisContext)
   setImageNav(imgHeader)
-  const establecEntretenimiento = useGetData("asistencia")
+  const { asistencia } = useGetData(["asistencia"])
   return (
     <section className="w-[90%]">
       <h1 className="font-bold text-3xl text-center text-[#14A44D] my-4">
         Asistencias
       </h1>
       <article className="w-full flex items-center justify-center flex-row gap-5 flex-wrap mb-8">
-        {establecEntretenimiento?.map((data) => (
+        {asistencia?.map((data) => (
           <Card key={data.id_asistencias} data={data} />
         ))}
       </article>
